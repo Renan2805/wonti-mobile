5ffdff32e1fbcf314c3bfc9ec5e5c2ba68a75039
unknown <vitorhugolima19@hotmail.com>
2022-08-27 20:53:11 -0300

    Arrumando as medidas

--- screens/ConfigScreen.tsx
+++ screens/ConfigScreen.tsx
@@ -8,113 +8,113 @@ type Props = NativeStackScreenProps<RootStackParamList>
 const ConfigScreen = ({ navigation }: Props) => {
   return (
     <ScrollView contentContainerStyle={style.content}>
-      <View style={{width:'100%', padding:'2rem'}}>
+      <View style={{width:'100%', padding:'32px'}}>
         <Text style={style.TextConta}>Conta</Text>
       </View>
-      <View style={{width:'100%',flexDirection:'row', display:'flex', paddingLeft:'2rem'}}>
+      <View style={{width:'100%',flexDirection:'row', display:'flex', paddingLeft:'32px'}}>
         <View style={{
-         padding:'2rem', 
-         width:'9vh',
-         height:'9vh',
+         padding:'32px', 
+         width:'81px',
+         height:'80px',
          borderRadius:100, 
          borderWidth:1}}>
-        </View>
+      </View>
         <TouchableOpacity style={style.ButtonView}>
           <Text style={style.TextNome}>Alana Moreira</Text>
           <Text style={style.TextDesc}>Informações da conta</Text>
-          <View style={{display:'flex', position:'absolute', left:'15rem',top:'2rem'}}>
+          <View style={{display:'flex', position:'absolute', left:'75%',top:'32px'}}>
            <IoIosArrowForward size={20} style={style.Flecha}/>
           </View>
         </TouchableOpacity>
       </View>
-      <View style={{width:'100%', padding:'2rem'}}>
+      <View style={{width:'100%', padding:'32px'}}>
         <Text style={style.TextConta}>Geral</Text>
       </View>
-      <View style={{width:'100%',flexDirection:'row',marginBottom:'1rem', display:'flex', paddingLeft:'2rem'}}>
+      <View style={{width:'100%',flexDirection:'row',marginBottom:'16px', display:'flex', paddingLeft:'32px'}}>
         <View style={{
-         padding:'2rem', 
-         width:'7vh',
-         height:'7vh',
+         padding:'32px', 
+         width:'63px',
+         height:'63px',
          borderRadius:100, 
          borderWidth:1}}>
         </View>
         <TouchableOpacity style={style.ButtonView}>
           <Text style={style.TextNome}>Notificações</Text>
-          <View style={{display:'flex', position:'absolute', left:'15rem',top:'1rem'}}>
+          <View style={{display:'flex', position:'absolute', left:'75%',top:'16px'}}>
            <IoIosArrowForward size={20} style={style.Flecha}/>
           </View>
         </TouchableOpacity>
       </View>
-      <View style={{width:'100%',flexDirection:'row',marginBottom:'1rem', display:'flex', paddingLeft:'2rem'}}>
+      <View style={{width:'100%',flexDirection:'row',marginBottom:'16px', display:'flex', paddingLeft:'32px'}}>
         <View style={{
-         padding:'2rem', 
-         width:'7vh',
-         height:'7vh',
+         padding:'32px', 
+         width:'63px',
+         height:'63px',
          borderRadius:100, 
          borderWidth:1}}>
         </View>
         <TouchableOpacity style={style.ButtonView}>
           <Text style={style.TextNome}>Vagas salvas</Text>
-          <View style={{display:'flex', position:'absolute', left:'15rem',top:'1rem'}}>
+          <View style={{display:'flex', position:'absolute', left:'75%',top:'16px'}}>
            <IoIosArrowForward size={20} style={style.Flecha}/>
           </View>
         </TouchableOpacity>
       </View>
-      <View style={{width:'100%',flexDirection:'row',marginBottom:'1rem', display:'flex', paddingLeft:'2rem'}}>
+      <View style={{width:'100%',flexDirection:'row',marginBottom:'16px', display:'flex', paddingLeft:'32px'}}>
         <View style={{
-         padding:'2rem', 
-         width:'7vh',
-         height:'7vh',
+         padding:'32px', 
+         width:'63px',
+         height:'63px',
          borderRadius:100, 
          borderWidth:1}}>
         </View>
         <TouchableOpacity style={style.ButtonView}>
           <Text style={style.TextNome}>Privacidade</Text>
-          <View style={{display:'flex', position:'absolute', left:'15rem',top:'1rem'}}>
+          <View style={{display:'flex', position:'absolute', left:'75%',top:'16px'}}>
            <IoIosArrowForward size={20} style={style.Flecha}/>
           </View>
         </TouchableOpacity>
       </View>
-      <View style={{width:'100%',flexDirection:'row',marginBottom:'1rem', display:'flex', paddingLeft:'2rem'}}>
+      <View style={{width:'100%',flexDirection:'row',marginBottom:'16px', display:'flex', paddingLeft:'32px'}}>
         <View style={{
-         padding:'2rem', 
-         width:'7vh',
-         height:'7vh',
+         padding:'32px', 
+         width:'63px',
+         height:'63px',
          borderRadius:100, 
          borderWidth:1}}>
         </View>
         <TouchableOpacity style={style.ButtonView}>
           <Text style={style.TextNome}>Segurança</Text>
-          <View style={{display:'flex', position:'absolute', left:'15rem',top:'1rem'}}>
+          <View style={{display:'flex', position:'absolute', left:'75%',top:'16px'}}>
            <IoIosArrowForward size={20} style={style.Flecha}/>
           </View>
         </TouchableOpacity>
       </View>
-      <View style={{width:'100%',flexDirection:'row',marginBottom:'1rem', display:'flex', paddingLeft:'2rem'}}>
+      <View style={{width:'100%',flexDirection:'row',marginBottom:'16px', display:'flex', paddingLeft:'32px'}}>
         <View style={{
-         padding:'2rem', 
-         width:'7vh',
-         height:'7vh',
+         padding:'32px', 
+         width:'63px',
+         height:'63px',
          borderRadius:100, 
          borderWidth:1}}>
         </View>
         <TouchableOpacity style={style.ButtonView}>
           <Text style={style.TextNome}>Ajuda e suporte</Text>
-          <View style={{display:'flex', position:'absolute', left:'15rem',top:'1rem'}}>
+          <View style={{display:'flex', position:'absolute', left:'75%',top:'16px'}}>
            <IoIosArrowForward size={20} style={style.Flecha}/>
           </View>
         </TouchableOpacity>
-      </View><View style={{width:'100%',flexDirection:'row', display:'flex',marginBottom:'2rem', paddingLeft:'2rem'}}>
+      </View><View style={{width:'100%',flexDirection:'row', display:'flex',marginBottom:'32px', paddingLeft:'32px'}}>
         <View style={{
-         padding:'2rem', 
-         width:'7vh',
-         height:'7vh',
+         padding:'32px', 
+         width:'63px',
+         height:'63px',
          borderRadius:100, 
          borderWidth:1}}>
         </View>
         <TouchableOpacity style={style.ButtonView}>
           <Text style={style.TextNome}>Sair</Text>
-          <View style={{display:'flex', position:'absolute', left:'15rem',top:'1rem'}}>
+          <View style={{display:'flex', position:'absolute', left:'75%',top:'16px'}}>
            <IoIosArrowForward size={20} style={style.Flecha}/>
           </View>
         </TouchableOpacity>
@@ -140,7 +140,7 @@ const style = StyleSheet.create({
     width:'100%'
   },
   ButtonView: {
-    padding:'1rem',
+    padding:'16px',
     flex:1,
     display:'flex',
   },
